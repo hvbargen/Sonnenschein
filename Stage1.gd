aca65f691c6b0e7309d197947809feb788def2ff
hvbargen <henning@von-bargen.eu>
2020-05-10 15:20:48 +0200

    Added a timer. Because the font used is not fixed size, alignment to the decimal point is tricky. Added experimental tile set for terrain. Assets imported are from "Pixel Adventure 1" and "Pixel Adventure 2" by Pixel Frog.

--- Stage1.gd
+++ Stage1.gd
@@ -1,8 +1,20 @@
 extends Node
 
+var time : float
 
 func _on_Bunny_score_changed(score):
 	$ScoreLabel.text = "%d" % score
 
 func _on_Wurzel_ready():
 	$Bunny.set_score(0)
+	time = 0.0
+	
+func _process(delta):
+	time += delta
+	print("xx%4.1fxx" % time)
+	var textlist = ("%6.1f" % time).split(".")
+	var secs:String = textlist[0]
+	var fracs:String = textlist[1]
+	print(secs,".",fracs)
+	$TimeHBox/TimeLabelSec.text = secs.strip_edges() + "."
+	$TimeHBox/TimeLabelFrac.text = fracs
